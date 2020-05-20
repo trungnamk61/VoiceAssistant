7c461da97e8d0dc448356c9d29ad011137f25ee1
trungnamk61 <namtrungnguyen299@gmail.com>
2020-05-20 23:31:15 +0700

    nam push

--- checkVoice.py
+++ checkVoice.py
@@ -1,74 +1,95 @@
 import speech_recognition as sr
 import os
 import time
-def parent_child(pid): 
-    n = os.fork() 
-  
-    # n greater than 0  means parent process 
-    if n > 0: 
-        pid = os.getpid()
-def length_file() :
-	File = open("/home/trungnam/Documents/OS_project/VoiceCommandProject/command.txt","a+")
-	Counter = 0
-	# Reading from file 
-	Content = File.read() 
-	CoList = Content.split("\n")
-	for i in CoList :
-		Counter+=1;
-    	#print(Counter-1);
-	File.close
-	return(Counter-1) 
-def main():
-	length_command = length_file()/2;
-	x = 0;
-	y = 0;
-	i = 0;
-	command = []
-	execute = []
-	count = -6;
-	count2 = 0;
-	error = 0;
-	file = open("/home/trungnam/Documents/OS_project/VoiceCommandProject/command.txt","a+")
-	for x in range(length_command) :
-		command.append(str(file.readline()).replace("\n",""))
-		execute.append(file.readline().replace("\n",""))
-	#print(command)
-	#print(execute)
-	r = sr.Recognizer()
-	while True :
+
+
+class speech :
+	start = 0
+	end = 0
+	text = " "
+	def length_file(self) :
+		File = open("command.txt","a+")
+		Counter = 0
+		# Reading from file 
+		Content = File.read() 
+		CoList = Content.split("\n")
+		for i in CoList :
+			Counter+=1;
+    		#print(Counter-1);
+		File.close
+		return(Counter-1) 
+	def speech2text(self) :
+		end = 0
+		r = sr.Recognizer()
+		#while True :
 		with sr.Microphone() as source :
-			r.adjust_for_ambient_noise(source) 
+			r.adjust_for_ambient_noise(source) 	 #Adjusts the energy threshold dynamically
 			#print("                       start : ",time.localtime(time.time()))
-			print ("Say something")
-			audio = r.listen(source)
+			print ("I'm listening")
+			audio = r.listen(source)			 #Records a single phrase from ``source`` (an ``AudioSource`` instance) into an ``AudioData`` instance
 			print("Time over,thanks")
 			#print("                       end : " ,time.localtime(time.time()))
 		try : 
 			print("text : " + r.recognize_google(audio));
 		except KeyboardInterrupt :
-			print("exit !!!");
+			print("exit !!!")
 			quit()
-		except :
+		except : 
 			print("Don't recognition your voice !!! Please talking again !")
 		else :
 			text = r.recognize_google(audio)
-			if (text == "hey") :
+			if (text == "hey Siri" or end == 1) :
+				start = 1
 				print("Xin chao !!!")
+			if (text == "goodbye") :
+				print("Goodbye !!!")
+				start = 0
+				end = 1
+		
+def main():
+	x = 0
+	y = 0
+	i = 0
+	command = []
+	execute = []
+	count = -6
+	count2 = 0
+	error = 0
+	Text = speech()
+	length_command = Text.length_file()/2
+
+	file = open("command.txt","a+")
+	for x in range(length_command) :
+		command.append(str(file.readline()).replace("\n",""))
+		execute.append(str(file.readline().replace("\n","")))
+	#print(command)
+	#print(execute)
+	
+	while True :
+		Text.speech2text()
+		print (Text.text)
+		if Text.start == 1 :
 			for y in range(length_command) :
 				count = count+1
 				#print (command[count])
-				if (text == command[count]) :
-					print('a')
+				if Text.text == command[count] :
+					print("a")
+					print('Executing...')
 					os.system(execute[count])
-
+					#print(parent_child)
+					print("Done !")
 				else :
 					error +=1
 				if error == length_command :
 					print("Don't execute your command voice!")
-			count = -6;
-			y=0;
-			error = 0;
+				count = -6
+				y=0;
+				error = 0
+		if Text.end == 1 :
+			quit()
 	file.close
-	del f,file,r,audio,command,execute,count,length_command,error
-if __name__ == "__main__":
-    main()
+main()
+start = 0
+end = 0
+text = "trungnam"
+del f,file,r,audio,command,execute,count,length_command,error,source,start,end,text
